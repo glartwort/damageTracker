a2a95ce74f754a233d8f26d9bd88af0d4d78520e
glartwort <glartwort@hotmail.com>
2025-08-30 14:58:22 -0700

    Added mostDmgTaken

--- scripts/damagetracker.js
+++ scripts/damagetracker.js
@@ -13,7 +13,6 @@ Hooks.on("clientSettingChanged", (settingKey, newValue) => {
   }
 });
 
-
 Hooks.on("createChatMessage", (message, data, options, userId) => {
   if (isDebug) console.log(LOG_PREFIX, "createChatMessage call detected");
 
@@ -23,43 +22,7 @@ Hooks.on("createChatMessage", (message, data, options, userId) => {
 
   //Get actual damage being applied
   if (message.flags.pf2e.context?.type === "damage-taken") {
-    const isNPCLoggingEnabled =  game.settings.get(MODULE_ID, "enableNPCTracking");
-    let attackerId =  message.flags.pf2e.origin?.actor;
-    const attackActor = (attackerId)?fromUuidSync(attackerId):null;
-    const attacker =    (attackActor)?attackActor.name:UNIDENTIFIED_ATTACKER;       //Probably should use token.name here for NPCs (to get decorated names) - should I lose attackerId (I don't use it to render)
-    let isNPC =       (attackActor?.type !== "character");
-
-    if (!attackerId) {  //invalid attackerId usually indicates manually applied damage (or unarmed strikes)
-      isNPC =       false;      // since it might be from a PC, we need to treat it as such
-      attackerId =  UNIDENTIFIED_ATTACKER;  //give it a value since checking a null key isn't good
-    }
-
-    const damageRegex = /span>\stakes\s(\d+)\sdamage./;
-    const damageString = message.content;
-    const validUpdate = message.flags.pf2e.appliedDamage?.updates;  //if missing, 0 damage done - process to get damageRoll anyway
-    const damage =      (validUpdate)?validUpdate[0].value:0;
-    const victim =      fromUuidSync(message.flags.pf2e.appliedDamage?.uuid)?.name;
-        
-    if ((!isNPC) || (isNPCLoggingEnabled)) {
-      let damageRoll = 0;
-      const dmgMatch = damageString.match(damageRegex);
-
-      if (dmgMatch) {
-            damageRoll = parseInt(dmgMatch[1],10);
-      }
-      
-      if (isDebug) {
-        console.log(LOG_PREFIX, "Damage Taken!");
-        console.log(LOG_PREFIX, "damage Amount: \t", damageRoll);
-        console.log(LOG_PREFIX, "damage: \t", damage);
-        console.log(LOG_PREFIX, "attacker: \t", attacker);
-        console.log(LOG_PREFIX, "attacker is NPC?:", isNPC)
-        console.log(LOG_PREFIX, "victim: \t", victim);
-        console.log(LOG_PREFIX, "track NPCs?", isNPCLoggingEnabled);
-      }
-
-      AddOrMergeActor(attackerId, attacker, isNPC, damageRoll, damage);
-    }
+    ProcessDamageTaken(message,data)
   }
 
   //Stash damage rolls for later reference (when damage is applied)
@@ -68,36 +31,7 @@ Hooks.on("createChatMessage", (message, data, options, userId) => {
   //  2. I'm using the attackerId (same as above) as the key to store the data, but I'm 
   //        logging the token name (instead of the actor.name).
   if (message.flags.pf2e.context?.type === "damage-roll") {
-    const isNPCLoggingEnabled =  game.settings.get(MODULE_ID, "enableNPCTracking");
-    const attackerTokenId = message.flags.pf2e.context?.token;
-    const attackerTokenName = canvas.tokens.get(attackerTokenId).name;
-    let attackerId =  message.flags.pf2e.origin?.actor;
-    const attackActor = (attackerId)?fromUuidSync(attackerId):null;
-    let isNPC =       (attackActor?.type !== "character");
-
-    if (!attackerId) {  //invalid attackerId usually indicates manually applied damage (or unarmed strikes)
-      isNPC =       false;      // since it might be from a PC, we need to treat it as such
-      attackerId =  UNIDENTIFIED_ATTACKER;  //give it a value since checking a null key isn't good
-    }
-
-    if ((!isNPC) || (isNPCLoggingEnabled)) {
-
-      //accumulate all damage roll totals.. not sure this is what we want vs. take largest.
-      let damageRoll = 0;
-      message.rolls.forEach(r => {
-        damageRoll = Math.max(damageRoll,r.total);
-      });
-            
-      if (isDebug) {
-        console.log(LOG_PREFIX, "Damage Taken!");
-        console.log(LOG_PREFIX, "damage Amount: \t", damageRoll);
-        console.log(LOG_PREFIX, "attacker token name: \t", attackerTokenName);
-        console.log(LOG_PREFIX, "attacker is NPC?:", isNPC)
-        console.log(LOG_PREFIX, "track NPCs?", isNPCLoggingEnabled);
-      }
-
-      StashDamageRoll(attackerId, attackerTokenName, isNPC, damageRoll);
-    }
+    ProcessDamageRoll(message, data);
   }
 });
 
@@ -115,39 +49,141 @@ Hooks.on("updateChatMessage", (message, data, options, userId) => {
 
     if (data.flags.pf2e.appliedDamage.isReverted)
     {
-      if (isDebug) console.log(LOG_PREFIX, "Revert damage found");
-
-      let attackerId =  message.flags.pf2e.origin?.actor;
-      const attackActor = (attackerId)?fromUuidSync(attackerId):null;
-      let isNPC =       (attackActor?.type !== "character");
-
-      if (!attackerId) {  //invalid attackerId usually indicates manually applied damage (or unarmed strikes)
-        isNPC =       false;                  //since it might be from a PC, we need to treat it as such
-        attackerId =  UNIDENTIFIED_ATTACKER;  //give it a value since checking a null key isn't good
-      }
-
-      const damageRegex = /span>\stakes\s(\d+)\sdamage./;
-      const damageString = data.content;
-      const damage =      message.flags.pf2e.appliedDamage.updates[0].value;
-        
-              
-      let damageRoll = 0;
-      const dmgMatch = damageString.match(damageRegex);
-
-      if (!dmgMatch) 
-         return;
-        
-      damageRoll = parseInt(dmgMatch[1],10);
-        
-      if (isDebug) {
-        console.log(LOG_PREFIX, "Damage Reverted!");
-        console.log(LOG_PREFIX, "damage: \t", damage);
-      }
-
-      RevertDamage(attackerId, damageRoll, damage);
+      ProcessDamageRevert(message, data);
     }
   }
 });
+
+function ProcessDamageTaken(message,data) {
+  const isNPCLoggingEnabled =  game.settings.get(MODULE_ID, "enableNPCTracking");
+  let attackerId =      message.flags.pf2e.origin?.actor;
+  const attackActor =   (attackerId)?fromUuidSync(attackerId):null;
+  const attacker =      (attackActor)?attackActor.name:UNIDENTIFIED_ATTACKER;       //Probably should use token.name here for NPCs (to get decorated names) - should I lose attackerId (I don't use it to render)
+  let isNPC =           (attackActor?.type !== "character");
+  const targetId =      message.flags.pf2e?.appliedDamage?.uuid;
+  const targetActor =   (targetId)?fromUuidSync(targetId):null;
+  let target = UNIDENTIFIED_ATTACKER;
+  if (targetActor.token)
+    target = targetActor.token.name;    //use token name if it's available
+  else
+    target = targetActor.name;          //fall back to actor name if it's not.
+
+  const targetIsNPC =  (targetActor?.type !== "character");   
+
+  if (!attackerId) {  //invalid attackerId usually indicates manually applied damage (or unarmed strikes)
+    isNPC =       false;      // since it might be from a PC, we need to treat it as such
+    attackerId =  UNIDENTIFIED_ATTACKER;  //give it a value since checking a null key isn't good
+  }
+
+  const damageRegex = /span>\stakes\s(\d+)\sdamage./;
+  const damageString = message.content;
+  const validUpdate = message.flags.pf2e.appliedDamage?.updates;  //if missing, 0 damage done - process to get damageRoll anyway
+  const damage =      (validUpdate)?validUpdate[0].value:0;
+  
+      
+  if ((!isNPC) || (isNPCLoggingEnabled)) {
+    let damageRoll = 0;
+    const dmgMatch = damageString.match(damageRegex);
+
+    if (dmgMatch) {
+          damageRoll = parseInt(dmgMatch[1],10);
+    }
+    
+    if (isDebug) {
+      console.log(LOG_PREFIX, "Damage Taken!");
+      console.log(LOG_PREFIX, "damage Amount: \t", damageRoll);
+      console.log(LOG_PREFIX, "damage: \t", damage);
+      console.log(LOG_PREFIX, "attacker: \t", attacker);
+      console.log(LOG_PREFIX, "attacker is NPC?:", isNPC)
+      console.log(LOG_PREFIX, "victim: \t", victim);
+      console.log(LOG_PREFIX, "track NPCs?", isNPCLoggingEnabled);
+    }
+
+    AddOrMergeActor(attackerId, attacker, isNPC, damageRoll, damage);
+
+    if ((!targetIsNPC) || (isNPCLoggingEnabled)) {
+      AddorMergeActorMostDmgTaken(targetId, target, targetIsNPC, damage);
+    }
+  }
+}
+
+function ProcessDamageRoll(message, data) {
+  const isNPCLoggingEnabled =  game.settings.get(MODULE_ID, "enableNPCTracking");
+  const attackerTokenId = message.flags.pf2e.context?.token;
+  const attackerTokenName = canvas.tokens.get(attackerTokenId).name;
+  let attackerId =  message.flags.pf2e.origin?.actor;
+  const attackActor = (attackerId)?fromUuidSync(attackerId):null;
+  let isNPC =       (attackActor?.type !== "character");
+
+  if (!attackerId) {  //invalid attackerId usually indicates manually applied damage (or unarmed strikes)
+    isNPC =       false;      // since it might be from a PC, we need to treat it as such
+    attackerId =  UNIDENTIFIED_ATTACKER;  //give it a value since checking a null key isn't good
+  }
+
+  if ((!isNPC) || (isNPCLoggingEnabled)) {
+
+    //accumulate all damage roll totals.. not sure this is what we want vs. take largest.
+    let damageRoll = 0;
+    message.rolls.forEach(r => {
+      damageRoll = Math.max(damageRoll,r.total);
+    });
+          
+    if (isDebug) {
+      console.log(LOG_PREFIX, "Damage Roll!");
+      console.log(LOG_PREFIX, "damage Amount: \t", damageRoll);
+      console.log(LOG_PREFIX, "attacker token name: \t", attackerTokenName);
+      console.log(LOG_PREFIX, "attacker is NPC?:", isNPC)
+      console.log(LOG_PREFIX, "track NPCs?", isNPCLoggingEnabled);
+    }
+    StashDamageRoll(attackerId, attackerTokenName, isNPC, damageRoll);
+  }
+}
+
+function ProcessDamageRevert(message,data) {
+  if (isDebug) console.log(LOG_PREFIX, "Revert damage found");
+  
+  const isNPCLoggingEnabled =  game.settings.get(MODULE_ID, "enableNPCTracking");
+  let attackerId =  message.flags.pf2e.origin?.actor;
+  const attackActor = (attackerId)?fromUuidSync(attackerId):null;
+  let isNPC =       (attackActor?.type !== "character");
+  const targetId =      message.flags.pf2e?.appliedDamage?.uuid;
+  const targetActor =   (targetId)?fromUuidSync(targetId):null;
+  let target = UNIDENTIFIED_ATTACKER;
+  if (targetActor.token)
+    target = targetActor.token.name;    //use token name if it's available
+  else
+    target = targetActor.name;          //fall back to actor name if it's not.
+
+  const targetIsNPC =  (targetActor?.type !== "character");   
+
+  if (!attackerId) {  //invalid attackerId usually indicates manually applied damage (or unarmed strikes)
+    isNPC =       false;                  //since it might be from a PC, we need to treat it as such
+    attackerId =  UNIDENTIFIED_ATTACKER;  //give it a value since checking a null key isn't good
+  }
+
+  const damageRegex = /span>\stakes\s(\d+)\sdamage./;
+  const damageString = data.content;
+  const damage =      message.flags.pf2e.appliedDamage.updates[0].value;
+    
+  let damageRoll = 0;
+  const dmgMatch = damageString.match(damageRegex);
+
+  if (!dmgMatch) 
+      return;
+    
+  damageRoll = parseInt(dmgMatch[1],10);
+    
+  if (isDebug) {
+    console.log(LOG_PREFIX, "Damage Reverted!");
+    console.log(LOG_PREFIX, "damage: \t", damage);
+  }
+
+  RevertDamage(attackerId, damageRoll, damage);
+
+   if ((!targetIsNPC) || (isNPCLoggingEnabled)) {
+      RevertMostDmgTaken(targetId, damage);
+    }
+}
 
 async function StashDamageRoll(key, name, isNPC, damageRoll) {
   const actorMap = game.settings.get(MODULE_ID, "damageMap") ?? {};
@@ -166,10 +202,9 @@ async function StashDamageRoll(key, name, isNPC, damageRoll) {
       // short circuit, no changes are required, don't bother setting data
       return;
     }
-    if (isDebug) console.log(LOG_PREFIX, "Merged data into existing", (isNPC)?"NPC":"PC", "for:", existing.name, "with", damageRoll, "damage.");
+    if (isDebug) console.log(LOG_PREFIX, "Merged data into existing", (isNPC)?"NPC":"PC", "for:", name, "with", damageRoll, "damage.");
   }
   await game.settings.set(MODULE_ID, "damageMap", actorMap);
-  
 }
 
 
@@ -191,6 +226,26 @@ function checkAndUpdateMaxDmgRoll(actor,damageRoll) {
   }
   return false;
 }
+
+// Return true if something changed, otherwise false.
+function checkAndUpdateMostDmgTaken(actor,damage) {
+  let currentMax = (actor.mostDmgTaken)?actor.mostDmgTaken:0;
+  
+  if (damage > currentMax) {
+    actor.prevMostDmgTaken = currentMax;
+    actor.mostDmgTaken = damage;
+    return true;
+  } 
+  // if the roll isn't bigger than the current max, but it is bigger than previous, update previous max.
+  // Explicity not doing anything on damageRoll == currentMax since this potentially gets called twice
+  // for damaging someone (damage roll, then damage hit).. If I recorded both it wouldn't revert (max and previous would be the same).
+  if ((damage > actor.prevMostDmgTaken) && (damage < currentMax)) {
+    actor.prevMostDmgTaken = damage;
+    return true;
+  }
+  return false;
+}
+
 
 // Don't need to return a value, anytime maxDmg is checked they'll be updating totDmg
 function checkAndUpdateMaxDmg(actor,damage) {
@@ -224,14 +279,11 @@ async function AddOrMergeActor(key, name, isNPC, damageRoll, damage) {
     actorMap[key].maxDmg = damage;
     actorMap[key].prevMaxDmg = 0;
     actorMap[key].totDmg = damage;
-
-    const Actortype = (isNPC)?"NPC":"PC";
-    
+  
     if (isDebug) console.log(LOG_PREFIX, "Created new", (isNPC)?"NPC":"PC", "for:", name, "with", damage, "damage.");
+
   } else {    //actor already exists, update
     const actor = actorMap[key];
-    const Actortype = (actor.isNPC)?"NPC":"PC";
-    
     let absMaxDmg = Math.max(damageRoll,damage);  //if damage is > damageRoll, use that.. 
 
     checkAndUpdateMaxDmgRoll(actor,absMaxDmg);
@@ -241,6 +293,33 @@ async function AddOrMergeActor(key, name, isNPC, damageRoll, damage) {
     
     if (isDebug) console.log(LOG_PREFIX, "Merged data into existing", (isNPC)?"NPC":"PC", "for:", actor.name, "with", damage, "damage.");
   }
+  await game.settings.set(MODULE_ID, "damageMap", actorMap);
+}
+
+async function AddorMergeActorMostDmgTaken(key, name, isNPC, damage) {
+  const actorMap = game.settings.get(MODULE_ID, "damageMap") ?? {};
+    
+  if (!actorMap[key]) {   
+    // Create new actor in damageMap.  
+    // This should be rare, the damage roll should have happened before applying damage 
+    // (in which case StashDamageRoll should have created the actor) and given it name = token.name.
+    // If this does create an actor it will be using actor.name (which wil be fine for linked actors).
+    actorMap[key] = {};
+    actorMap[key].name = name;
+    actorMap[key].isNPC = isNPC;
+    actorMap[key].mostDmgTaken = damage;
+    actorMap[key].prevMostDmgTaken = 0;
+    
+    if (isDebug) console.log(LOG_PREFIX, "Created new", (isNPC)?"NPC":"PC", "for:", name, "has taken", damage, "damage.");
+  } else {    //actor already exists, update
+    const existing = actorMap[key];
+    if (!checkAndUpdateMostDmgTaken(existing, damage)) {
+      // short circuit, no changes are required, don't bother setting data
+      return;
+    }
+      
+    if (isDebug) console.log(LOG_PREFIX, "Merged data into existing", (isNPC)?"NPC":"PC", "for:", name, "has taken", damage, "damage.");
+  } 
   await game.settings.set(MODULE_ID, "damageMap", actorMap);
 }
 
@@ -256,7 +335,6 @@ async function RevertDamage(key, damageRoll, damage) {
     if (actor.maxDmgRoll == damageRoll) {
       actor.maxDmgRoll = actor.prevMaxDmgRoll;
       actor.prevMaxDmgRoll = 0;     //don't save the current or it could never revert that roll.  Shouldn't happen, but just in case.
-
     }    
     
     if (actor.maxDmg == damage) {
@@ -268,8 +346,26 @@ async function RevertDamage(key, damageRoll, damage) {
 
     if (isDebug) console.log(LOG_PREFIX, "Reverted damage from existing", (actor.isNPC)?"NPC":"PC", "for:", actor.name, "with", damage, "damage.");
   
-    await game.settings.set(MODULE_ID, "damageMap", actorMap);
-    
+    await game.settings.set(MODULE_ID, "damageMap", actorMap); 
   }
-  
+} 
+
+async function RevertMostDmgTaken(key, damage) {
+  const actorMap = game.settings.get(MODULE_ID, "damageMap") ?? {};
+    
+  if (!actorMap[key]) {  
+    if (isDebug) console.log(LOG_PREFIX, "Actor is not in the table to revert most damage taken from");
+  } else {    //actor already exists, update
+    const actor = actorMap[key];
+    const Actortype = (actor.isNPC)?"NPC":"PC";
+    
+    if (actor.mostDmgTaken == damage) {
+      actor.mostDmgTaken = actor.prevMostDmgTaken;
+      actor.prevMaxDmgRoll = 0;     //don't save the current or it could never revert that roll.  Shouldn't happen, but just in case.
+      if (isDebug) console.log(LOG_PREFIX, "Reverted damage from existing", (actor.isNPC)?"NPC":"PC", "for:", actor.name, "with", damage, "damage.");
+
+      await game.settings.set(MODULE_ID, "damageMap", actorMap); 
+    }
+  }                  
 }
+
